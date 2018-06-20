8a4d15fcfb1e0fc325f269346b9716eb43ff99f0
Daniel <daniel@developerdan.com>
2018-06-20 09:32:19 -0400

    Updated file headers to include license. Full update on list.

--- HostsTools/hosts_tools.py
+++ HostsTools/hosts_tools.py
@@ -5,9 +5,31 @@ from typing import List, Set
 import re
 import requests
 import json
+import datetime
 
 STRIP_COMMENTS_PATTERN = re.compile(r"^([^#]+)")
 ALLOWED_DOMAIN_PATTERN = re.compile("(?!-)[A-Z\d-]***REMOVED***1,255***REMOVED***(?<!-)$", re.IGNORECASE)
+FILE_HEADER = """
+# Collection of Analytics, Ads, and tracking hosts to block.
+# 
+# Details: https://github.com/lightswitch05/hosts
+# Issues: https://github.com/lightswitch05/hosts/issues
+# Source: https://raw.githubusercontent.com/lightswitch05/hosts/master/[file_name]
+#
+# Copyright [yyyy] Daniel White
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.\n
+""".lstrip().replace('[yyyy]', str(datetime.datetime.now().year))
 
 
 def sort_domains(domains: List[str]) -> List[str]:
@@ -50,8 +72,7 @@ def load_domains_from_list(file_name: str) -> Set[str]:
 def write_domain_list(file_name: str, domains: Set[str]):
     sorted_domains = sort_domains(list(domains))
     with open(file_name, 'w') as file:
-        file.write('# Collection of Analytics, Ads, and tracking hosts to block\n')
-        file.write('# https://github.com/lightswitch05/hosts/blob/master/%s\n\n' % file_name)
+        file.write(FILE_HEADER.replace('[file_name]', file_name))
         for domain in sorted_domains:
             file.write('0.0.0.0 %s\n' % domain)
 
