25e6db4630c0329d5242c7118d802b4813279578
Daniel <daniel@developerdan.com>
2018-11-27 16:02:35 -0500

    Pull current branch before merging in master.

--- cron.bash
+++ cron.bash
@@ -2,6 +2,7 @@
 git fetch
 git checkout .
 git checkout automatic-updates
+git pull
 git pull --ff-only origin master
 python3 update_hosts_file.py -u -v
 python3 update_hosts_file.py -l amp-hosts -u -v
