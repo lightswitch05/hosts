a57e51d08949acc0efbd5d110f6e525be3f1690f
Daniel <daniel@developerdan.com>
2019-03-12 12:24:52 -0400

    Improved cron script.

--- cron.bash
+++ cron.bash
@@ -7,7 +7,7 @@ git pull --ff-only origin master
 python3 update_hosts_file.py -u -v
 python3 update_hosts_file.py -l amp-hosts -u -v
 python3 update_hosts_file.py -l tracking-aggressive -u -v
-git add ads-and-tracking-extended.txt tracking-aggressive-extended.txt amp-hosts-extended.txt
+git add *extended.txt docs/lists/*extended.txt
 git commit -m "Automatic list update"
 git push
 
