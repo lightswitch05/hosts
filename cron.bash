656ece16bc558a4e8fd75aa0104c6c3ea78f3d64
Daniel <daniel@developerdan.com>
2019-04-04 09:04:21 -0400

    Update cron to not over-use virus total

--- cron.bash
+++ cron.bash
@@ -4,9 +4,14 @@ git checkout .
 git checkout automatic-updates
 git pull
 git pull --ff-only origin master
-python3 update_hosts_file.py -u -v
-python3 update_hosts_file.py -l amp-hosts -u -v
-python3 update_hosts_file.py -l tracking-aggressive -u -v
+EVEN_DAY=$((($(date +%u)+0)%2))
+if [ $EVEN_DAY -eq 0 ];
+then
+    python3 update_hosts_file.py -u -v
+else
+    python3 update_hosts_file.py -l amp-hosts -u -v
+    python3 update_hosts_file.py -l tracking-aggressive -u -v
+fi
 git add *extended.txt docs/lists/*extended.txt
 git commit -m "Automatic list update"
 git push
