da8992be4e4cde6680b3a118f386f60c0ad29bc5
Daniel <daniel@developerdan.com>
2019-11-15 09:05:27 -0500

    Update cron script to more reliably update all list types

--- cron.bash
+++ cron.bash
@@ -4,18 +4,20 @@ git checkout ./**/*.txt *.txt ./docs/index.html
 git checkout automatic-updates
 git pull
 git pull --ff-only origin master
-EVEN_DAY=$((($(date +%u)+0)%2))
-if [ $EVEN_DAY -eq 0 ];
-then
+LIST_TYPE=$1
+
+if [ "$LIST_TYPE" == "primary" ]; then
     echo "Updating primary list"
     python3 update_hosts_file.py -u -v
-else
+elif [ "$LIST_TYPE" == "secondary" ]; then
     echo "Updating secondary lists"
     python3 update_hosts_file.py -l amp-hosts -u -v
     python3 update_hosts_file.py -l tracking-aggressive -u -v
     python3 update_hosts_file.py -l facebook -u -v
+else
+    echo "Unknown list time, please supply either 'primary' or 'secondary'"
 fi
+
 git add *extended.txt docs/lists/*extended.txt docs/index.html
 git commit -m "Automatic list update"
 git push
-
